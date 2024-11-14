55987dba15d777907afa32cb9a0fdd6f353847c9
Chrystian de Almeida Silva <chrystian.almeida@alunoescolassociais.org.br>
2024-11-14 07:17:43 -0300

    Escutador do tipo submit, que faz a conversão com consumo de API (Exchangerate)

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 //   -----------------------------------------------------------------------------------------------------------
 // CONSULTA DE API COM TAXAS DE CÂMBIO 
-const apiKey = "c1aec72e7e30ee5797dbf838"; //Chave da Api 
+const apiKey = 'c1aec72e7e30ee5797dbf838'; //Chave da Api 
 const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
 
-// Função para consultar a taxa de câmbio via api
+// Function to check the exchange rate via API
 
-async function getExchangeRates (inCoins, ForCoins) {
+async function getExchangeRates(ofCoins, forCoins) {
     try {
-        const response = await fetch(`${apiUrl}${inCoins}`);
-        const data = response.json();
-        
-        if(data === "success"){
-            return data.conversion_rates[ForCoins];
+        const response = await fetch(`${apiUrl}${ofCoins}`);
+        const data = await response.json();
+
+        if (data.result === "success") {
+            return data.conversion_rates[forCoins];
         } else {
-            throw new Error ("Erro ao buscar taxa de câmbio");
+            throw new Error("Erro ao buscar taxa de câmbio");
         }
-    }catch (error){
+    } catch (error) {
         console.error("Error", error);
         return null;
     }
 }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+// Adiciona escutador de eventos do tipo submit
+document.getElementById("Form-convert").addEventListener("submit", async function (event) {
+
+    event.preventDefault();//Parar atualização da página
+
+    // Obter valores de entrada
+    const value = parseFloat(document.getElementById("value-amount").value); // Obter o valor inserido pelo usuário e convertê-lo para número decimal
+    const ofCoins = document.getElementById("ofCoins").value; // Obter o tipo de moeda de origem selecionado
+    const forCoins = document.getElementById("forCoins").value; // Obter o tipo de moeda de destino selecionado
+    
+    // Passa as moedas selecionadas para o esquema condicional
+    const ExchangeRate = await getExchangeRates(ofCoins, forCoins);
+    // Guarda valor 
+    if(ExchangeRate) {
+        const convertedValue = value * ExchangeRate;
+
+        // Pega id do campo onde será exibido mensagem com o valor convetido 
+        const converty = document.getElementById("conversao");
+        converty.textContent = `Resultado: ${convertedValue.toFixed(2)} ${forCoins}`;
+    } else {
+        alert("Error fetching quote, Please try again");
+    }
+});
